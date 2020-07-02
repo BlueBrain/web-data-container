e82cf3c33171229d6197bc575c9b86a87a536132
jonathanlurie <lurie.jo@gmail.com>
2020-07-02 15:57:15 +0200

    VERSION bump
    Change-Id: Ie0a3dd9d4d87d53d0174b5066fed4aa921506019

--- src/randomaccessbuffer/__init__.py
+++ src/randomaccessbuffer/__init__.py
@@ -1,5 +1,3 @@
 from randomaccessbuffer.RandomAccessBuffer import RandomAccessBuffer
 from randomaccessbuffer.RandomAccessBuffer import TYPES
 from randomaccessbuffer.RandomAccessBuffer import __version__
-
-# __version__ = "0.1.1"
